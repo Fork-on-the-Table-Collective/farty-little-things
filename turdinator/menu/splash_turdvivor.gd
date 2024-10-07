4a061c07d914b99d3bc7e3dc19975ae99a8476c9
Weroosh <83233754+Weroosh@users.noreply.github.com>
2024-10-07 18:35:38 +0200

    Sequence for splash screen added
    With fade in and fade out
    Every splash scene has own code -- sorry bad architecture
    Next scene is set in every individual scene
    Project starts with Fott logo

--- turdinator/menu/splash_turdvivor.gd
+++ turdinator/menu/splash_turdvivor.gd
@@ -1,13 +1,38 @@
 extends Control
 
+@onready var in_time : float = 0.5
+@onready var fade_in_time : float = 1.5
+@onready var showing_time : float = 4.0
+@onready var fade_out_time : float = 1.5
+@onready var out_time : float = 0.3
+
+@onready var pushing_label_pulsation_time : float = 0.5
+
+@onready var next_scene = "res://menu/skin_select.tscn"
+
+@onready var screen = $TextureRect
+@onready var pushing_label = $Label
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#implement a few secods of delay to show the game splash screen, then redirect to the vegan selector
-	#create pulsing on "pushing..." text if it not possible by animation
-	pass # Replace with function body.
-
+	#ToDo: Implement fade in, fade out, and transfer to the next scene automatically (splash_turdvivor)
+	screen.modulate.a = 0.0
+	fade()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func fade() -> void:
+	var tween = self.create_tween()
+	tween.tween_interval(in_time)
+	tween.tween_property(screen, "modulate:a", 1.0,fade_in_time)
+	tween.tween_interval(showing_time)
+	tween.tween_property(screen, "modulate:a", 0.0,fade_out_time)
+	tween.tween_interval(out_time)
+	await tween.finished
+	
+	get_tree().change_scene_to_file(next_scene)
+	
