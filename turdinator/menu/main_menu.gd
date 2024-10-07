ff3f66118b30cf43d554d9d88be7decaa4746124
hus89 <ahus89@gmail.com>
2024-10-07 23:46:03 +0200

    fasza

--- turdinator/menu/main_menu.gd
+++ turdinator/menu/main_menu.gd
@@ -11,6 +11,9 @@ func _on_button_start_pressed() -> void:
 func _on_button_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu/settings_menu.tscn")
 
+func _on_button_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu/ty_for_playing.tscn")
+
 func _on_button_exit_pressed() -> void:
 	get_tree().quit()
 
