1b061545ec8f086658cedc2ead8ca45b1cfb3d13
GuBee33 <74045047+GuBee33@users.noreply.github.com>
2024-10-07 16:35:53 +0200

    volume silder updated

--- turdinator/scenes/common/volume_slider.gd
+++ turdinator/scenes/common/volume_slider.gd
@@ -7,14 +7,10 @@ var bus_index: int
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
-
-	value = db_to_linear(
-		AudioServer.get_bus_volume_db(bus_index)
-	)
-	Global.slider_value_dict[bus_name] = value
+	value=Global.slider_value_dict[bus_name]
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
 
 func _on_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(
-		bus_index,
-		linear_to_db(value)
-	)
+	Global.slider_value_dict[bus_name]=value
+	Global.store_variables()
+	Global.set_volume_by_bus(bus_name)
