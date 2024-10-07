f33cdca917b40a026057c6085f8501e79fe4810b
GuBee33 <74045047+GuBee33@users.noreply.github.com>
2024-10-07 19:02:57 +0200

    javítás a paraméterken meg a patrolon

--- turdinator/entities/enemy/enemy.gd
+++ turdinator/entities/enemy/enemy.gd
@@ -5,9 +5,12 @@ const PATROL_TURNOVER_TIME = 5.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var attack_effect: AnimatedSprite2D = $AnimatedSprite2D/AttackEffect
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var perception_shape: CollisionShape2D = $PerceptionArea/CollisionShape2D
+@onready var hit_box: CollisionShape2D = $HitBoxArea/HitBox
 
-@export var speed = 200.0
-@export var damage = -20.0
+var speed = 200.0
+var damage = -20.0
 @export var enemy:Enemies
 @export var is_patroling:bool=true
 
@@ -22,6 +25,7 @@ var cooldown_timer: Timer
 var patrol_timer: Timer
 var is_click_on_cooldown: bool = false
 var origin_speed
+var go=true
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
@@ -35,54 +39,74 @@ enum Enemies {
 	Sinoc,
 	Croissant
 }
-var enemies_char_sheets=[
+const ENEMIES_CHAR_SHEETS=[
 	{
 		"path":"res://entities/assets/Fly_anim_sheet_new.png",
 		"rows":1,
 		"cols":3,
-		"scale":Vector2(.3,.3),
+		"speed":250.0,
+		"damage":-5.0,
+		"scale":Vector2(.1,.1),
 		"attak_position":Vector2(-137,153),
 		"attak_scale":Vector2(0.7,0.7),
-		"attak_rotation":-125.0
+		"attak_rotation":-125.0,
+		"collision_radius": 20.0,
+		"perception_radius":100.0,
+		"hitbox_radius":20.0,
 	},
 	{
 		"path":"res://entities/assets/Rat_anim_sheet_new.png",
 		"rows":1,
 		"cols":3,
+		"speed":200.0,
+		"damage":-20.0,
 		"scale":Vector2(.3,.3),
 		"attak_position":Vector2(-202,102.5),
 		"attak_scale":Vector2(0.6,0.56),
-		"attak_rotation":130.0
+		"attak_rotation":130.0,
+		"collision_radius": 18.0,
+		"perception_radius":30.0,
+		"hitbox_radius":22.0,
 	},
 	{
 		"path":"res://entities/assets/sinoc_sheet.png",
 		"rows":1,
 		"cols":3,
+		"speed":200.0,
+		"damage":-10.0,
 		"scale":Vector2(.4,.4),
 		"attak_position":Vector2(37.5,67.5),
 		"attak_scale":Vector2(0.332,0.427),
-		"attak_rotation":-147.0
+		"attak_rotation":-147.0,
+		"collision_radius": 10.0,
+		"perception_radius":20.0,
+		"hitbox_radius":13.0,
 	},
 	{
 		"path":"res://entities/assets/Croissant_anim_sheet.png",
 		"rows":2,
 		"cols":2,
+		"speed":100.0,
+		"damage":-25.0,
 		"scale":Vector2(.4,.4),
 		"attak_position":Vector2(-120,77.5),
 		"attak_scale":Vector2(0.35,0.35),
-		"attak_rotation":-125.0
+		"attak_rotation":-125.0,
+		"collision_radius": 10.0,
+		"perception_radius":20.0,
+		"hitbox_radius":10.0,
 	},
 ]
 
 func set_animation_from_images():
-	var sprite_sheet:Image=load(enemies_char_sheets[enemy].path).get_image()
+	var sprite_sheet:Image=load(ENEMIES_CHAR_SHEETS[enemy].path).get_image()
 	var sprite_frames = SpriteFrames.new()
 	animated_sprite.frames = sprite_frames
-	var frame_width = sprite_sheet.get_size().x/enemies_char_sheets[enemy].cols
-	var frame_height  = sprite_sheet.get_size().y/enemies_char_sheets[enemy].rows
+	var frame_width = sprite_sheet.get_size().x/ENEMIES_CHAR_SHEETS[enemy].cols
+	var frame_height  = sprite_sheet.get_size().y/ENEMIES_CHAR_SHEETS[enemy].rows
 	#sprite_frames.add_animation("default")
-	for row in range(enemies_char_sheets[enemy].rows):
-		for col in range(enemies_char_sheets[enemy].cols):
+	for row in range(ENEMIES_CHAR_SHEETS[enemy].rows):
+		for col in range(ENEMIES_CHAR_SHEETS[enemy].cols):
 			var frame_rect = Rect2(col * frame_width, row * frame_height, frame_width, frame_height)
 			var texture = ImageTexture.create_from_image(sprite_sheet.get_region(frame_rect))
 			sprite_frames.add_frame("default",texture)
@@ -91,14 +115,24 @@ func set_movement(t: Node2D):
 	velocity = (t.global_position - position).normalized() * speed
 
 func _ready() -> void:
-	self.scale=enemies_char_sheets[enemy].scale
+	speed=ENEMIES_CHAR_SHEETS[enemy].speed
+	damage=ENEMIES_CHAR_SHEETS[enemy].damage
 	set_animation_from_images()
-	attack_effect.position=enemies_char_sheets[enemy].attak_position
-	attack_effect.rotation_degrees=enemies_char_sheets[enemy].attak_rotation
-	attack_effect.scale=enemies_char_sheets[enemy].attak_scale
-	print(attack_effect.position)
-	print(attack_effect.rotation_degrees)
-	print(attack_effect.scale)
+	attack_effect.position=ENEMIES_CHAR_SHEETS[enemy].attak_position
+	attack_effect.rotation_degrees=ENEMIES_CHAR_SHEETS[enemy].attak_rotation
+	attack_effect.scale=ENEMIES_CHAR_SHEETS[enemy].attak_scale
+	var c_shape:CircleShape2D=collision_shape.shape.duplicate()
+	c_shape.radius=ENEMIES_CHAR_SHEETS[enemy].collision_radius
+	var p_shape:CircleShape2D=perception_shape.shape.duplicate()
+	p_shape.radius=ENEMIES_CHAR_SHEETS[enemy].perception_radius
+	var h_shape:CircleShape2D=hit_box.shape.duplicate()
+	h_shape.radius=ENEMIES_CHAR_SHEETS[enemy].hitbox_radius
+	self.scale=ENEMIES_CHAR_SHEETS[enemy].scale
+	collision_shape.shape=c_shape
+	perception_shape.shape=p_shape
+	hit_box.shape=h_shape
+
+	
 	origin_speed = speed
 	velocity = Vector2.ONE
 	cooldown_timer = Timer.new()
@@ -113,17 +147,13 @@ func _ready() -> void:
 	patrol_timer.timeout.connect(_on_Patrol_timeout)
 	add_child(patrol_timer)  # Add the timer to the scene
 	patrol_timer.start()
+	patrol()
 
-func _process(_delta: float) -> void:
-	if direction==Vector2(0,0):
-		patrol()
-
+	
 
 func _on_Patrol_timeout():
-	orientation=randi() % 2
 	patrol()
-	#direction*=-1
-
+	
 func _on_Cooldown_timeout():
 	speed=origin_speed
 	patrol()
@@ -131,7 +161,8 @@ func _on_Cooldown_timeout():
 	attack_effect.visible = false
 
 func _physics_process(_delta: float) -> void:
-	
+	if ray_cast_right.is_colliding() or ray_cast_left.is_colliding() or ray_cast_up.is_colliding() or ray_cast_down.is_colliding() : 
+		patrol()
 	if target != null:
 		set_movement(target)
 	if velocity.x >0:
@@ -143,20 +174,12 @@ func _physics_process(_delta: float) -> void:
 func patrol() -> void:
 	if is_patroling:
 		velocity = Vector2.ONE
-		direction=Vector2.ONE
-		if orientation == 0:
-			direction.y=0
-			if ray_cast_right.is_colliding() :
-				direction.x= -1.0
-			elif ray_cast_left.is_colliding() :
-				direction.x = 1.0
-		elif orientation == 1:
-			direction.x=0
-			if ray_cast_up.is_colliding() :
-				direction.y= 1
-			elif ray_cast_down.is_colliding() :
-				direction.y = -1
-		velocity = direction * speed
+		direction = Vector2(randf_range(-1,1),randf_range(-1,1))
+		velocity = direction.normalized() * speed /2
+		
+	print("dir: "+str(direction))
+	print("velo: "+str(velocity))
+	print("speed: "+str(speed))
 
 
 func _on_hit_box_area_body_entered(_body: Node2D) -> void:
