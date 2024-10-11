e800d6f4ef3098da98cddfa107be4ea36e57f15e
GuBee33 <74045047+GuBee33@users.noreply.github.com>
2024-10-11 20:29:24 +0200

    adding pause menu and removed exit button from player screen

--- turdinator/menu/pause_menu.gd
+++ turdinator/menu/pause_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
-
 func _on_button_exit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://menu/main_menu.tscn")
 
 
 func _on_button_resume_pressed() -> void:
-	#ToDo: implement continue game here
-	pass # Replace with function body.
+	get_tree().paused = false
+	queue_free()  # Remove the pause menu when resuming
